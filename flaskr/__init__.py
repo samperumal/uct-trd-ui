b2ad1ba12daf0dec3027254298db5ddd08060917
Sam Perumal <sam_lki@yahoo.com>
2019-11-20 15:35:05 +0200

    Configured SqlLite DB

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,7 +3,6 @@ import pydim
 
 from flask import Flask
 
-
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
@@ -45,9 +44,12 @@ def create_app(test_config=None):
         # Register listeners
         pydim.dic_info_service("ztt_dimfed_server_trd-fee_00_2_0_STATE", updateGenericInt("state"))
 
-# a simple page that says hello
+    # a simple page that says hello
     @app.route('/hello')
     def hello():
-        return 'Hello, World!'
+        return 'Hello, World!'    
+
+    from . import db
+    db.init_app(app)
 
     return app
